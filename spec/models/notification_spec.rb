6dcbb3e4981403aecbef39b90281eceb94f0444a
Vern Kofford <cheow3@gmail.com>
2018-08-07 15:18:22 -0600

    Refactored create method for FactoryGirl for faster creation

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Notification, type: :model do
   describe 'creation' do
 
     it 'can be created' do
-      notification = FactoryGirl.create(:notification)
+      notification = FactoryGirl.build_stubbed(:notification)
       expect(notification).to be_valid
     end
 
   end
-  
+
 end
